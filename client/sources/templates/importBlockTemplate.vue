b7e09257dfd92e5b1648bd88d572a0ec7a8c8b1b
Balun Vladimir <balun2010@mail.ru>
2019-01-14 18:15:19 +0300

    Improved import form and added structure of other components

--- client/sources/templates/importBlockTemplate.vue
+++ client/sources/templates/importBlockTemplate.vue
@@ -2,19 +2,47 @@
 
     <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 block import-form">
         <p class="block-title">Creating nesting order</p>
-        <textarea class="block-textarea" placeholder="Nesting request only in JSON format..."></textarea>
-        <select class="block-select">
-            <option>localhost:8080</option>
+        <textarea class="block-textarea" placeholder="Nesting request only in JSON format..." v-model="nestingRequest"></textarea>
+        <select class="block-select" v-model="availableServers">
+            <option v-for="server in availableServers">{{ server.name }}</option>
         </select>
-        <button class="block-button">Send Nesting Request</button>
+        <button class="block-button" @click="onClickSendNestingRequest">Send Nesting Request</button>
     </div>
 
 </template>
 
 <script>
+
+    import configuration from '../resources/data/configuration'
+
     export default {
-        name: "importBlockTemplate"
+        name: "importBlockTemplate",
+        data: function () {
+            return {
+                availableServers: configuration.availableServers,
+                nestingRequest: ''
+            }
+        },
+        methods: {
+
+            onClickSendNestingRequest: () => {
+                if (!isValidJSONText(this.nestingRequest)) {
+                    alert("Nesting request is incorrect.");
+                }
+            }
+
+        }
     }
+    
+    function isValidJSONText(jsonText) {
+        try {
+            JSON.parse(jsonText);
+        } catch (error) {
+            return false;
+        }
+        return true;
+    }
+
 </script>
 
 <style scoped>
---
+++ client/sources/templates/visualizationBlockTemplate.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "visualizationBlockTemplate"
+    }
+</script>
+
+<style scoped>
+
+</style>
