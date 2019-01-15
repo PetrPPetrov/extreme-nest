3688c844dedde495798f534d08bdc952944a9a3f
Balun Vladimir <balun2010@mail.ru>
2019-01-15 10:30:03 +0300

    Added primitive interaction with server from client

--- client/sources/templates/importBlockTemplate.vue
+++ client/sources/templates/importBlockTemplate.vue
@@ -3,15 +3,25 @@
     <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 block import-form">
         <p class="block-title">Creating nesting order</p>
         <textarea class="block-textarea" placeholder="Nesting request only in JSON format..." v-model="nestingRequest"></textarea>
-        <select class="block-select" v-model="availableServers">
+        <select v-model="selectedAvailableServer" class="block-select">
             <option v-for="server in availableServers">{{ server.name }}</option>
         </select>
-        <button class="block-button" @click="onClickSendNestingRequest">Send Nesting Request</button>
+        <button class="block-button"
+                @click="onClickSendNestingRequest"
+                :class="{'disabled-block-button' : isRunningSendingRequest}"
+                :disabled="isRunningSendingRequest">
+            Send Nesting Request
+        </button>
+        <div>
+            <p class="log-message">{{ message }}</p>
+        </div>
     </div>
 
 </template>
 
 <script>
+
+    import axios from 'axios'
 
     import configuration from '../resources/data/configuration'
 
@@ -20,20 +30,46 @@
         data: function () {
             return {
                 availableServers: configuration.availableServers,
-                nestingRequest: ''
+                selectedAvailableServer: 'Not selected',
+                nestingRequest: '',
+                isRunningSendingRequest : false,
+                message: '...'
             }
         },
         methods: {
 
-            onClickSendNestingRequest: () => {
+            onClickSendNestingRequest: function() {
+                if (this.nestingRequest.length === 0) {
+                    this.message = 'Error. Nesting request is empty.';
+                    return;
+                }
+
+                if ( (this.selectedAvailableServer.length === 0) ||
+                     (this.selectedAvailableServer === 'Not selected') ) {
+                    this.message = 'Error. Did not select available server.';
+                    return;
+                }
+
                 if (!isValidJSONText(this.nestingRequest)) {
-                    alert("Nesting request is incorrect.");
+                    this.message = 'Error. Nesting request is incorrect.'
+                } else {
+                    this.message = 'Request are handling...';
+                    this.isRunningSendingRequest = true;
+                    const address = this.selectedAvailableServer + '/new';
+                    sendNestingRequest(address, this.nestingRequest)
+                        .then((data) => {
+                            alert(data);
+                        }).catch((error) => {
+                            this.message = 'Error. Connection with selected server was not set.';
+                        });
+                    this.isRunningSendingRequest = false;
+                    this.message = "Nesting order was handled successfuly."
                 }
             }
 
         }
     }
-    
+
     function isValidJSONText(jsonText) {
         try {
             JSON.parse(jsonText);
@@ -43,8 +79,33 @@
         return true;
     }
 
+    function sendNestingRequest(address, nestingRequest) {
+        return new Promise((resolve, reject) => {
+            axios({
+                method : 'post',
+                url : "http://127.0.0.1:8080/new",
+                data : address,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+                    'Access-Control-Allow-Headers': 'Origin, Content-Type',
+                    'Accept': 'application/json; charset=utf-8',
+                    'Content-type': 'application/json; charset=utf-8'
+                },
+            }).then(response => {
+                resolve(response.data);
+            }).catch(error => {
+                reject(false);
+            });
+        });
+    }
+
 </script>
 
 <style scoped>
+
+    .import-form div {
+        display: inline-block;
+    }
 
 </style>
