3c9ac2fad39b8d1a8d73104a6b7603166c42553d
Balun Vladimir <balun2010@mail.ru>
2019-03-11 15:26:23 +0300

    Improved generation algorithm on the test client

--- test-client/sources/scripts/goldNestingGenerator.js
+++ test-client/sources/scripts/goldNestingGenerator.js
@@ -5,9 +5,45 @@
 
 'use strict';
 
+import * as _ from 'underscore';
+
 const generateGoldNestingAsync = async (countFigures, sheetWidth, sheetHeight, nestingTime) => {
     return generateNesting(countFigures, sheetWidth, sheetHeight, nestingTime);
 };
+
+function Rectangle(width, height, yPosition, xPosition) {
+    this.width = width;
+    this.height = height;
+    this.yPosition = yPosition;
+    this.xPosition = xPosition;
+}
+
+function divideRectangle(rectangle, countParts, geometry){
+    if (countParts === 0) {
+        return;
+    }
+    if (countParts === 1) {
+        geometry.push(rectangle);
+        return;
+    }
+
+    const leftCountParts = Math.floor(countParts / 2);
+    const rightCountParts = countParts - leftCountParts;
+    let newWidth = rectangle.width;
+    let newHeight = rectangle.height;
+    if (rectangle.width > rectangle.height)
+    {
+        newWidth /= 2;
+        divideRectangle(new Rectangle(newWidth, newHeight, rectangle.yPosition, rectangle.xPosition), leftCountParts, geometry);
+        divideRectangle(new Rectangle(newWidth, newHeight, rectangle.yPosition, rectangle.xPosition + newWidth), rightCountParts, geometry);
+    }
+    else
+    {
+        newHeight /= 2;
+        divideRectangle(new Rectangle(newWidth, newHeight, rectangle.yPosition, rectangle.xPosition), leftCountParts, geometry);
+        divideRectangle(new Rectangle(newWidth, newHeight, rectangle.yPosition + newHeight, rectangle.xPosition), rightCountParts, geometry);
+    }
+}
 
 function generateNesting(countFigures, sheetWidth, sheetHeight, nestingTime) {
     const nestingRequest = {};
@@ -25,6 +61,11 @@ function generateNesting(countFigures, sheetWidth, sheetHeight, nestingTime) {
         'length': sheetWidth,
         'id': sheetID
     });
+
+    const geometry = [];
+    const rootRectangle = new Rectangle(sheetWidth, sheetHeight, 0, 0);
+    divideRectangle(rootRectangle, countFigures, geometry);
+    console.log(geometry);
 
     const figureID = 11; //TODO: need to generate id
     const rectangleHeight = (sheetHeight / countFigures) * 2;
