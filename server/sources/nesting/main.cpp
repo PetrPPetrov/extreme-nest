27727a927f4266b5e79cc746b9cf45a35ac3a154
Petr Petrov <petr@gkmsoft.ru>
2019-03-12 10:16:34 +0300

    Added nesting method command line parameter

--- server/sources/nesting/main.cpp
+++ server/sources/nesting/main.cpp
@@ -21,19 +21,34 @@ int main(int argc, char** argv)
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
 
+    std::string nesting_method = "nfp";
+
     if (argc < 2)
     {
         std::cout << generateJson(
-            Error("Insufficient command line arguments, Extreme Nest usage: extreme_nest nesting_request.json"), 
+            Error("Insufficient command line arguments, Extreme Nest usage: extreme_nest nesting_request.json [nesting_method]"), 
             "Extreme Nest Copyright (c) 2019 by GkmSoft") << std::endl;
         return EXIT_FAILURE;
+    }
+
+    if (argc >= 3)
+    {
+        nesting_method = argv[2];
     }
 
     try
     {
         NestingRequest::Order nesting_order(argv[1]);
         nesting_task_ptr nesting_task = generateTask(nesting_order);
-        nesting_result_ptr nesting_result = Nfp::Optimization().nest(nesting_task);
+        nesting_result_ptr nesting_result;
+        if (nesting_method == "nfp")
+        {
+            nesting_result = Nfp::Optimization().nest(nesting_task);
+        }
+        else
+        {
+            nesting_result = Pr::Optimization().nest(nesting_task);
+        }
         std::string result_json = generateJson(*nesting_result, *nesting_task);
         std::cout << result_json << std::endl;
     }
